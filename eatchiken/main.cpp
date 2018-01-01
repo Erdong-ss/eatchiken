4ad6e9fd32ead7ce2239e631edfaa9ce282b25e1
Valar <770348252@QQ.COM>
2018-01-02 07:41:17 +0800

    Initial Commit

---
+++ eatchiken/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eatchiken
+//
+//  Created by Edong on 2018/1/2.
+//  Copyright © 2018年 Edong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
